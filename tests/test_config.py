cb589580c06a82a5646bd6b860aeaf2b3b283905
Jesus Lara <jesuslarag@gmail.com>
2021-01-09 04:31:21 +0100

    testing navconfig

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,9 @@ os.environ['NAVCONFIG_DRIVE_CLIENT'] = 'credentials.txt'
 os.environ['NAVCONFIG_DRIVE_ID'] = file_id
 
 from navconfig import config, BASE_DIR, DEBUG
+from navconfig.conf import adwh_url, DEBUG, dwh_url, CACHE_URL
 
 print(config)
 print('base dir is: ', BASE_DIR)
 print('debug is: ', DEBUG)
+print('cache is: ', CACHE_URL)
