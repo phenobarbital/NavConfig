6e9e77c5f0f4aa34f7b28cce595d0c937395009d
Jesus Lara <jesuslarag@gmail.com>
2021-10-21 18:08:59 +0200

    added pyYAML

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'aiodns==3.0.0',
         'python-dotenv==0.15.0',
         'configparser==5.0.2',
+        'PyYAML>=6.0',
         'PyDrive==1.3.1',
         'pylibmc==1.6.1',
         'objectpath==0.6.1',
