974ba243165796c1d28bc254c2ccff8703c572cc
Jesus Lara <jesuslarag@gmail.com>
2022-08-02 23:00:19 +0200

    bump version of navconfig: security fixes

--- navconfig/version.py
+++ navconfig/version.py
@@ -3,7 +3,7 @@
 __title__ = 'navconfig'
 __description__ = ('Configuration tool for all Navigator Services '
                    'Tool for accessing Config info from different sources.')
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 __author__ = 'Jesus Lara'
 __author_email__ = 'jesuslarag@gmail.com'
 __license__ = 'BSD'
