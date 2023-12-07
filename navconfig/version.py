68e94f44d0bd04b70cc499301571755be27faedf
Jesus Lara <jesuslarag@gmail.com>
2023-12-07 18:44:08 +0100

    bump version of dependencies

--- navconfig/version.py
+++ navconfig/version.py
@@ -3,7 +3,7 @@
 __title__ = 'navconfig'
 __description__ = ('Configuration tool for all Navigator Services '
                    'Tool for accessing Config info from different sources.')
-__version__ = '1.4.0'
+__version__ = '1.5.0'
 __author__ = 'Jesus Lara'
 __author_email__ = 'jesuslarag@gmail.com'
 __license__ = 'MIT'
