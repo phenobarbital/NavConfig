ea930ba562790bc7d37dd4ed80c9f8b16d2fba61
Jesus Lara <jesuslarag@gmail.com>
2025-04-06 11:20:55 +0200

    fix version of redis and jsonpickle

--- navconfig/version.py
+++ navconfig/version.py
@@ -5,7 +5,8 @@ __description__ = (
     "Configuration tool for all Navigator Services "
     "Tool for accessing Config info from different sources."
 )
-__version__ = "1.7.12"
+__version__ = "1.7.13"
+__copyright__ = "Copyright (c) 2020-2024 Jesus Lara"
 __author__ = "Jesus Lara"
 __author_email__ = "jesuslarag@gmail.com"
 __license__ = "MIT"
